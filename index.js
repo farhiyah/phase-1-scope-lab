ba45490241f3a444bb8b3b45fc27fd701c0e3056
farhiya dahir <farhiya.dahir@student.moringaschool.com>
2022-08-22 20:19:44 +0300

    Completed assgnment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// const log = console.log;
+var customerName ='bob'
+
+const leastFavoriteCustomer = 'Queen';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return ;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = '';
+    return leastFavoriteCustomer;
+}
